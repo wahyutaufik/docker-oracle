cd69bedece1fe88cd4d7ed9b6694917f124c451a
Wahyu Taufik <wahyutaufik37@gmail.com>
2015-11-26 16:05:10 +0700

    remove something useless

--- oracle-install.sh
+++ oracle-install.sh
@@ -11,24 +11,6 @@ downloadOracle () {
 
 	cd / && curl -o oracle-xe_11.2.0-1.0_amd64.deb $url/master/oracle-xe_11.2.0-1.0_amd64.deb
 
-# 	local ora_deb_partial=(
-# 		${ORA_DEB}aa
-# 		${ORA_DEB}ab
-# 		${ORA_DEB}ac
-# 	)
-
-# 	local i=1
-# 	for part in "${ora_deb_partial[@]}"; do
-# 		echo "[Downloading '$part' (part $i/3)]"
-# 		curl -s --retry 3 -m 60 -o /$part -L $url/blob/master/assets/$part?raw=true
-# 		i=$((i + 1))
-
-# 	done
-
-# 	cat /${ORA_DEB}a* > /${ORA_DEB}
-
-# 	rm -f /${ORA_DEB}a*
-
 }
 
 downloadOracle
